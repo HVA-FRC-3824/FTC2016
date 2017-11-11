6cb3f5ad690e1a47ca2c7fc4db58d66443eb794e
Robert Palmer Jr <robert.palmer@ingenutec.com>
2017-11-11 13:26:40 -0500

    Cleaned up the drive functionality

--- TeamCodePromo/src/main/java/org/firstinspires/ftc/teamcodepromo/systems/Drive.java
+++ TeamCodePromo/src/main/java/org/firstinspires/ftc/teamcodepromo/systems/Drive.java
@@ -8,29 +8,40 @@ import org.firstinspires.ftc.teamcodepromo.hardwareDefinition.HvaHardwarePushbot
 
 public class Drive {
     private HvaHardwarePushbot bot;
+    private boolean orientForward;
 
     public enum Direction {CCW, CW};
 
     public Drive(HvaHardwarePushbot ourBot){
         bot = ourBot;
+        orientForward = false;
     }
 
     public void circle(Direction dir){
         if(dir == Direction.CW){
             // drive Clockwise - left wheels FASTER than right wheels
             // the greater the difference the tighter the circle
-            bot.leftMotor.setPower(0.5);
-            bot.rightMotor.setPower(0.25);
+            bot.leftMotor.setPower(-1.0);
+            bot.rightMotor.setPower(-0.0);
         }
         else{
             // drive Counter-Clockwise - left wheels SLOWER than right wheels
-            bot.leftMotor.setPower(0.25);
-            bot.rightMotor.setPower(0.5);
+            bot.leftMotor.setPower(-0.0);
+            bot.rightMotor.setPower(-1.0);
         }
     }
 
     public void tank(double left, double right){
-        bot.leftMotor.setPower(left);
-        bot.rightMotor.setPower(right);
+        if(orientForward)
+        {
+            // Joystick is NEGATIVE when pushed forward, so negate
+            bot.leftMotor.setPower(-left);
+            bot.rightMotor.setPower(-right);
+        }
+        else
+        {
+            bot.leftMotor.setPower(right);
+            bot.rightMotor.setPower(left);
+        }
     }
 }
